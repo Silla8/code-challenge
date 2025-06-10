7cf4aaf7c67204e188c2d912baf24ab8cfc71287
Silla Ibrahim (13040) <sibrahim13040@ada.edu.az>
2025-06-10 19:42:25 +0400

    deployment error fixing by removing all react/icons

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,5 @@
 import "../styles/ProgressBar.css"
-import { FaTruck } from "react-icons/fa6";
-import { RiDeleteBin5Line } from "react-icons/ri";
-import { GrLocation } from "react-icons/gr";
-import { FiShield } from "react-icons/fi";
-import { FaCalendar } from "react-icons/fa";
-import { FiCreditCard } from "react-icons/fi";
+
 
 export default function ProgressBar(){
 
@@ -12,32 +7,32 @@ export default function ProgressBar(){
     return (
         <div className="progress-bar">
             <div className="progress-item">
-                <GrLocation style={{ color: '#2563EB' }} size={30}/>
+                
                 <p>Postcode</p>
             </div>
             <div className="line-blue"/>
             <div className="progress-item">
-                <RiDeleteBin5Line style={{ color: '#2563EB' }} size={30}/>
+                
                 <p>Waste Type</p>
             </div>
             <div className="line-blue"/>
             <div className="progress-item">
-                <FaTruck style={{ color: '#2563EB' }} size={30}/>
+                
                 <p>Select Skip</p>
             </div>
             <div className="line-grey"/>
             <div className="progress-item">
-                <FiShield style={{ color: '#9CA3AF' }} size={30}/>
+                
                 <p>Permit Check</p>
             </div>
             <div className="line-grey"/>
             <div className="progress-item">
-                <FaCalendar style={{ color: '#9CA3AF' }} size={30}/>
+               
                 <p>Choose Date</p>
             </div>
             <div className="line-grey"/>
             <div className="progress-item">
-                <FiCreditCard style={{ color: '#9CA3AF' }} size={30}/>
+                
                 <p>Payment</p>
             </div>
         </div>
